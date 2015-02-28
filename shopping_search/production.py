abad53688debbb76cd37d94d60e7dbd5d13f4466
enkidulan <supamaxy@gmail.com>
2015-02-28 20:52:05 +0200

    updated production config

--- shopping_search/production.py
+++ shopping_search/production.py
@@ -2,3 +2,5 @@
 from shopping_search.settings import *
 
 FRONTEND_APP_PATH = os.path.join(PROJECT_ROOT, 'ngbp', 'bin')
+
+ALLOWED_HOSTS = [ '*' ]
--- shopping_search/wsgi.py
+++ shopping_search/wsgi.py
@@ -2,5 +2,8 @@ import os
 import sys
 
 
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
