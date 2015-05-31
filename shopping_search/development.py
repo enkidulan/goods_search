cb70fbbb7c640d76259fbac3cedcd3d191dafc08
Maksym Shalenyi <supamaxy@gmail.com>
2015-06-01 01:49:47 +0300

    added logging

--- shopping_search/development.py
+++ shopping_search/development.py
@@ -4,3 +4,41 @@ DEBUG=True
 TEMPLATE_DEBUG=DEBUG
 
 FRONTEND_APP_PATH = os.path.join(PROJECT_ROOT, 'ngbp', 'build')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'shopping_services.log',
+            'formatter': 'verbose'
+        },
+        'stream': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'shopping_search': {
+            'handlers': ['file', 'stream'],
+            'level': 'DEBUG',
+        },
+    }
+}
